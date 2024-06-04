4eed4a7aeead25163a63808a30cd015a0426d71a
ClaHugo <lulaporfavor@gmail.com>
2024-06-04 16:34:06 -0300

    Initial commit

---
+++ js/form.js
@@ -0,0 +1,5 @@
+function msg(){
+let nome = document.getElementById("Nome").value
+let idade = document.getElementById("idade").value
+    document.getElementById("receba").innerHTML = ` seu nome e + ${nome}` + `${idade}` 
+}
